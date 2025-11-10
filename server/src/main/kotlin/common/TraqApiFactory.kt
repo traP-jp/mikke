8deaeb637a08dadf3ab0eb10bb7822b0a9575c65
howard127 <60410935+howard12721@users.noreply.github.com>
2025-11-10 16:12:44 +0900

    server: Lint落ちてた

--- server/src/main/kotlin/common/TraqApiFactory.kt
+++ server/src/main/kotlin/common/TraqApiFactory.kt
@@ -4,13 +4,16 @@ import io.ktor.client.*
 import jp.trap.mikke.config.Environment
 import jp.trap.mikke.traq.client.infrastructure.ApiClient
 
-class TraqApiFactory(val httpClient: HttpClient) {
+class TraqApiFactory(
+    val httpClient: HttpClient,
+) {
     val baseUrl = Environment.TRAQ_API_BASE_URL
 
     inline fun <reified T : ApiClient> createApi(
         constructor: (String, HttpClient) -> T,
         token: String,
-    ): T = constructor(baseUrl, httpClient).apply {
-        setAccessToken(token)
-    }
+    ): T =
+        constructor(baseUrl, httpClient).apply {
+            setAccessToken(token)
+        }
 }
